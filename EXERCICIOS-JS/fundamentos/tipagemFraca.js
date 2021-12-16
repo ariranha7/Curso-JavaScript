7a08029bee3bbf53d377c5076f35e50b08b62945
ariranha7 <avdiegoreis@hotmail.com>
2021-12-16 13:22:30 -0300

    segundo dia de estudos

---
+++ EXERCICIOS-JS/fundamentos/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer =  34.454545;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evite nome genéricos e siglas
+let valor = '';
+let numero = 1;
+let pqp = false // projeto químico perigoso
