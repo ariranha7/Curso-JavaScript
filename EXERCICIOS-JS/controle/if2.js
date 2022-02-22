1a238596eb80d1804d520ec5479cee56600595b6
ariranha7 <avdiegoreis@hotmail.com>
2022-02-22 15:07:49 -0300

    nono dia de estudos

---
+++ EXERCICIOS-JS/controle/if2.js
@@ -0,0 +1,21 @@
+function teste1(num)
+{
+    if (num > 12)
+    {
+        console.log(num);
+        console.log("Final");
+    }
+}
+
+function teste2(num)
+{
+    if (num > 7)
+    {
+        console.log(num);
+    }
+}
+
+teste1(10);
+teste1(15);
+teste2(4);
+teste2(9);
---
+++ EXERCICIOS-JS/controle/ifElse.js
@@ -0,0 +1,15 @@
+const imprimirResultado = function(num)
+{
+    if (num >= 7)
+    {
+        console.log("Aprovado!");
+    }
+    else
+    {
+        console.log("Reprovado!");
+    }
+}
+
+imprimirResultado(7);
+imprimirResultado(10);
+imprimirResultado(3);
